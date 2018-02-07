23a765c8f754978b114768e25ddac3e6fd03731e
Garrett <Garrettcrg@gmail.com>
2018-02-06 18:07:19 -0600

    Finished

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import History from "../History/History";
+import Contact from "../Contact/Contact";
 export default class About extends Component {
   render() {
     return (
       <div>
-        <div className='subnav'>
+        <div className="subnav">
+          <Link to="/about" className="subnav">
+            <h3>About</h3>
+          </Link>
+          <Link to="/about/history" className="subnav">
+            <h3>History</h3>
+          </Link>
+          <Link to="/about/contact" className="subnav">
+            <h3>Contact</h3>
+          </Link>
         </div>
-        <div className='box'>
-        </div>
+
+        <div className="box" />
+        <Switch>
+          <Route Component={History} path="/about/history" />
+          <Route Component={Contact} path="/about/contact" />
+          <Route Component={About} path="/about" />
+        </Switch>
       </div>
-    )
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './main.css';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "./main.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
